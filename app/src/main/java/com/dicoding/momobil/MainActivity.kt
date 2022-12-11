3657b28b464d4bba14c5231d309eb9ff2308bafb
Bobby Jonathan <bobby.cool00763@protonmail.com>
2022-12-11 20:54:03 +0700

    set up stack navigations

--- app/src/main/java/com/dicoding/momobil/MainActivity.kt
+++ app/src/main/java/com/dicoding/momobil/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.dicoding.momobil.ui.theme.MomobilTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,22 +16,9 @@ class MainActivity : ComponentActivity() {
       MomobilTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          Greeting("Android")
+          MomobilApp()
         }
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-  MomobilTheme {
-    Greeting("Android")
   }
 }
--- app/src/main/java/com/dicoding/momobil/ui/screen/about/AboutScreen.kt
+++ app/src/main/java/com/dicoding/momobil/ui/screen/about/AboutScreen.kt
@@ -1,9 +1,57 @@
 package com.dicoding.momobil.ui.screen.about
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.dicoding.momobil.ui.theme.MomobilTheme
+import com.dicoding.momobil.ui.theme.TaxiSoftRed
 
 @Composable
-fun AboutScreen(modifier: Modifier = Modifier) {
+fun AboutScreen(
+  modifier: Modifier = Modifier,
+  navigation: NavHostController = rememberNavController()
+) {
+  Column(
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Column(
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Text(
+        text = "About Screen"
+      )
+      Button(
+        onClick = {
+          navigation.popBackStack("LandingPage", inclusive = false)
+        },
+        colors = ButtonDefaults.buttonColors(
+          backgroundColor = TaxiSoftRed
+        )
+      ) {
+        Text(
+          "To Product Detail",
+          color = Color.White
+        )
+      }
+    }
+  }
+}
 
+@Preview(showBackground = true)
+@Composable
+fun AboutScreenPreview() {
+  MomobilTheme {
+    AboutScreen()
+  }
 }
