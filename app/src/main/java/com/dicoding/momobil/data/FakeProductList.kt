72766c404ad851d23ac85ddfed56a25917ed195f
Bobby Jonathan <bobby.cool00763@protonmail.com>
2022-12-17 20:28:38 +0700

    change product image urls

--- app/src/main/java/com/dicoding/momobil/data/FakeProductList.kt
+++ app/src/main/java/com/dicoding/momobil/data/FakeProductList.kt
@@ -10,14 +10,14 @@ object FakeProductList {
       description = "<div>Honda CRV 1.5 Turbo Prestige AT 2019 Putih</div><div>Harga OTR Cash  = Rp. 480.000.000</div><div>Harga OTR Kredit  = Rp. 463.000.000</div><div>Cicilan Mulai dari Rp 9.5 jt/bln</div><div><br></div><div>Tersedia, Plat Mobil Ganjil</div><div><br></div><div>Dokumen  = Tidak wajib BBN</div><div><br></div><div>Mobil ini tipe tertinggi pada model CR-V, SUV andalan pabrikan Honda ini memiliki body yg lebih aerodinamis dan elegan, selain itu cabin yg cukup lapang membuat kenyamanan bagi anda yang mengendarainya. Fitur fitur dimobil ini juga terlengkap, pada stir terdapat handsfree-telephone, audiosteering switch dan Cruise Control yang membuat Anda lebih dapat berkonsentrasi saat berkendara. Jadi tunggu apalagi segera kunjungi kami untuk mendapatkan mobil ini!</div><div><br></div><div>Carro hadir untuk memberikan pelayanan pengiriman terbaik dan teraman yang menjangkau seluruh area Indonesia. Mobil akan diantarkan sesuai dengan kota tujuan Anda.</div><div><br></div><div>Garansi Mesin &amp; Transmisi 180 hari</div><div>Bebas Kecelakaan, Banjir</div><div>Dokumen Lengkap</div><div><br></div><div>Transmisi  = Automatic</div><div>Warna  = Putih</div><div>Kedaluarsa Pajak  = 04 Mar 2023</div><div>Kilometer  = 63.029 KM</div><div>BPKB Ada  = Ya</div><div>Tahun Produksi Kendaraan  = 2019</div><div>CC Mesin  = 1.500</div><div>Bahan Bakar  = Bensin</div><div><br></div><div>Melayani Pembelian via Cash atau kredit.</div><div><br></div><div>Cicilan Mulai dari Rp 9.5 jt/bln</div><div>Kelengkapan Data yang diperlukan  =</div><div>-Copy KTP Suami + Istri ( Jika Sudah Menikah )</div><div>-Copy Kartu Keluarga</div><div>-Copy PBB</div><div>-Rekening Listrik 3 bulan terakhir</div><div><br></div><div>Carro Automall</div><div>Jl. Harapan Indah Boulevard Commercial Park I Kavl CP 1, Kota Bekasi, Jawa Barat 17132</div><div><br></div><div>Carro Square Pondok Indah</div><div>Jl. Sultan Iskandar Muda No.41, RT.9/RW.4, Kby. Lama Utara, Kec. Kby. Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12240</div><div><br></div><div>Carro Automall Point</div><div>WTC Mall Matahari, Jl. Raya Serpong No.39, Pd. Jagung, Kec. Serpong,</div><div>Kota Tangerang Selatan, Banten 15326</div><div>Ruko No. 5863, Lobby Barat</div><div><br></div><div>Carro Automall Point Semarang</div><div>Semarang Town Square (Setos), Ground Floor</div><div>Jl. Inspeksi Jl. Gajahmada No.06, Kembangsari, Kec. Semarang Tengah, Kota Semarang, Jawa Tengah 50133</div><div><br></div><div>Buka Setiap Hari Senin - Jumat  = Pukul 9 Pagi s/d 5 Sore</div><div>Sabtu  = Pukul 9 Pagi s/d 3 Sore</div><div>Minggu  = Pukul 10 Pagi s/d 3 Sore</div><div><br></div><div>GMLQV2</div>",
       price = 463000000,
       images = listOf(
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/prod/img-p1648433231874-1_datcfo",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/prod/img-p1648433231874-2_cgyb2n",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/prod/img-p1648433231874-3_dsysza",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/prod/img-p1648433231874-4_tyexym",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/prod/img-p1648433231874-5_wtzgoe",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/prod/img-p1648433231874-6_pzoe1d",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/prod/img-p1648433231874-7_vnkzl2",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/prod/img-p1648433231874-8_i29psf"
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1648433231874-1_datcfo_ijda4c.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1648433231874-2_cgyb2n_wn7ea6.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1648433231874-3_dsysza_aetuse.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1648433231874-4_tyexym_sfceih.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1648433231874-5_wtzgoe_lyaapb.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1648433231874-6_pzoe1d_zglyze.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1648433231874-7_vnkzl2_vjthp3.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1648433231874-8_i29psf_bxys7t.jpg",
       ),
       fuel = "Bensin",
       year = 2019,
@@ -34,7 +34,9 @@ object FakeProductList {
       name = "DAIHATSU ASTREA",
       description = "qpwoeipoqwie",
       price = 25000000,
-      images = listOf("https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-244541-1_ngvcsj"),
+      images = listOf(
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244541-1_ngvcsj_rznbm6.jpg"
+      ),
       fuel = "Bensin",
       year = 2015,
       color = "Hitam",
@@ -50,7 +52,9 @@ object FakeProductList {
       name = "TOYOTA COROLLA",
       description = "mobil test corolla dx",
       price = 70000000,
-      images = listOf("https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-244527-1_wjuvrj"),
+      images = listOf(
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244527-1_wjuvrj_a7le28.jpg"
+      ),
       fuel = "Bensin",
       year = 2012,
       color = "Putih",
@@ -66,7 +70,9 @@ object FakeProductList {
       name = "SUZUKI APV GX",
       description = "",
       price = 205000000,
-      images = listOf("https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-198469-1_na1wqt"),
+      images = listOf(
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-198469-1_na1wqt_ldhfs7.png"
+      ),
       fuel = "Bensin",
       year = 2022,
       color = "Silver",
@@ -82,7 +88,9 @@ object FakeProductList {
       name = "HYUNDAI GRAND EUR XG AT",
       description = "New Car",
       price = 700000000,
-      images = listOf("https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-244525-1_rphxjx"),
+      images = listOf(
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244525-1_rphxjx_eumeeu.png"
+      ),
       fuel = "Bensin",
       year = 2021,
       color = "Hitam",
@@ -98,7 +106,9 @@ object FakeProductList {
       name = "HYUNDAI HYUNDA AVEGA GX M/T",
       description = "New Car",
       price = 700000000,
-      images = listOf("https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-244524-1_keo0ld"),
+      images = listOf(
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244524-1_keo0ld_yjmvku.png"
+      ),
       fuel = "Bensin",
       year = 2021,
       color = "Hitam",
@@ -115,11 +125,10 @@ object FakeProductList {
       description = "New Car",
       price = 280000000,
       images = listOf(
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-244105-2_svkeyh__1665113951487_151064",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-244105-3_oojrec",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-244105-4_qjh8jk",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-244105-5_zc2yus",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-244105-6_pgtyws"
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244105-2_svkeyh__1665113951487_151064_utp3zt.png",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244105-3_oojrec_kwoubj.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244105-4_qjh8jk_lrx6z8.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244105-5_zc2yus_uedagl.jpg",
       ),
       fuel = "Bensin",
       year = 2019,
@@ -137,11 +146,8 @@ object FakeProductList {
       description = "",
       price = 300000000,
       images = listOf(
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657614319522-1_mxqm0o",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657614319522-2_jkji7b",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657614319522-3_uzrczx",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657614319522-4_m6gi4i",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657614319522-5_lrde8u"
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1657614319522-1_mxqm0o_p8gxd5.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1657614319522-1_mxqm0o_p8gxd5.jpg",
       ),
       fuel = "Bensin",
       year = 2019,
@@ -159,11 +165,9 @@ object FakeProductList {
       description = "",
       price = 250000000,
       images = listOf(
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657612872226-1_il91pb",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657612872226-2_cda37w",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657612872226-3_ax71jq",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657612872226-4_kmx2ue",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657612872226-5_p5gf0i"
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1657614319522-1_mxqm0o_p8gxd5.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1657614319522-1_mxqm0o_p8gxd5.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1657614319522-1_mxqm0o_p8gxd5.jpg"
       ),
       fuel = "Bensin",
       year = 2017,
@@ -181,11 +185,9 @@ object FakeProductList {
       description = "",
       price = 400000000,
       images = listOf(
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657612346839-1_zv3hgd",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657612346839-2_i5ysng",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657612346839-3_exagpb",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657612346839-4_zpw6dc",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1657612346839-5_n6belc"
+        "https://res.cloudinary.com/lelestaticassets/image/upload/v1671282917/Dicoding%20Final%20Project/img-p1657612346839-1_zv3hgd_zhhcrc.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/v1671282917/Dicoding%20Final%20Project/img-p1657612346839-1_zv3hgd_zhhcrc.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/v1671282917/Dicoding%20Final%20Project/img-p1657612346839-1_zv3hgd_zhhcrc.jpg"
       ),
       fuel = "Diesel",
       year = 2015,
@@ -202,7 +204,9 @@ object FakeProductList {
       name = "TOYOTA AVANZA 1.3 G",
       description = "Testitng tambah monbil",
       price = 500000000,
-      images = listOf("https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-244346-1_s8w3ot"),
+      images = listOf(
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244346-1_s8w3ot_ctdfsw.jpg"
+      ),
       fuel = "Bensin",
       year = 2015,
       color = "Hitam",
@@ -219,11 +223,9 @@ object FakeProductList {
       description = "",
       price = 600000000,
       images = listOf(
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656566595960-1_ykk9og",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656566595960-2_qlr4yc",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656566595960-3_vcywvw",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656566595960-4_nvcp0f",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656566595960-5_w7gvar"
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244345-1_f6ovyx_ftenyn.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244345-1_f6ovyx_ftenyn.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244345-1_f6ovyx_ftenyn.jpg"
       ),
       fuel = "Bensin",
       year = 2014,
@@ -240,7 +242,9 @@ object FakeProductList {
       name = "TOYOTA AVANZA 1.3 E",
       description = "tsesyting ical",
       price = 340000000,
-      images = listOf("https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-244345-1_f6ovyx"),
+      images = listOf(
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244345-1_f6ovyx_ftenyn.jpg"
+      ),
       fuel = "Bensin",
       year = 2016,
       color = "Hitam",
@@ -257,11 +261,9 @@ object FakeProductList {
       description = "",
       price = 500000000,
       images = listOf(
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656562855640-1_ox4tb5",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656562855640-2_hwpcbf",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656562855640-3_akrezz",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656562855640-4_kfozmn",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656562855640-5_qqcuyc"
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1656562855640-3_akrezz_urg4hr.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1656562855640-3_akrezz_urg4hr.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1656562855640-3_akrezz_urg4hr.jpg"
       ),
       fuel = "Bensin",
       year = 2018,
@@ -279,11 +281,9 @@ object FakeProductList {
       description = "",
       price = 300000000,
       images = listOf(
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656062953831-1_g0tqjr",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656062953831-2_sf5lwi",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656062953831-3_lyqbdh",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656062953831-4_jc68ko",
-        "https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-p1656062953831-5_yltk94"
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1656062953831-3_lyqbdh_wtprlb.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1656062953831-3_lyqbdh_wtprlb.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1656062953831-3_lyqbdh_wtprlb.jpg"
       ),
       fuel = "Bensin",
       year = 2018,
@@ -300,7 +300,9 @@ object FakeProductList {
       name = "TOYOTA VIOS G A/T",
       description = "",
       price = 200000000,
-      images = listOf("https://res.cloudinary.com/adirafinance/image/upload/c_scale,h_560,dpr_auto,f_auto/media/products/dev/img-244106-1_fiov5v"),
+      images = listOf(
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244106-1_fiov5v_n0umqp.jpg"
+      ),
       fuel = "Bensin",
       year = 2019,
       color = "Silver",
