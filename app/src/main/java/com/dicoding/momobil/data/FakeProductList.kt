04e3cb4fc67b0e853a07ab3dcb4be0472e1a66f6
Bobby Jonathan <bobby.cool00763@protonmail.com>
2022-12-26 13:15:24 +0700

    set unique images for each data

--- app/src/main/java/com/dicoding/momobil/data/FakeProductList.kt
+++ app/src/main/java/com/dicoding/momobil/data/FakeProductList.kt
@@ -7,7 +7,6 @@ object FakeProductList {
     Mobil(
       id = 1,
       name = "HONDA CR-V 1.5 PRESTIGE TRB",
-      description = "<div>Honda CRV 1.5 Turbo Prestige AT 2019 Putih</div><div>Harga OTR Cash  = Rp. 480.000.000</div><div>Harga OTR Kredit  = Rp. 463.000.000</div><div>Cicilan Mulai dari Rp 9.5 jt/bln</div><div><br></div><div>Tersedia, Plat Mobil Ganjil</div><div><br></div><div>Dokumen  = Tidak wajib BBN</div><div><br></div><div>Mobil ini tipe tertinggi pada model CR-V, SUV andalan pabrikan Honda ini memiliki body yg lebih aerodinamis dan elegan, selain itu cabin yg cukup lapang membuat kenyamanan bagi anda yang mengendarainya. Fitur fitur dimobil ini juga terlengkap, pada stir terdapat handsfree-telephone, audiosteering switch dan Cruise Control yang membuat Anda lebih dapat berkonsentrasi saat berkendara. Jadi tunggu apalagi segera kunjungi kami untuk mendapatkan mobil ini!</div><div><br></div><div>Carro hadir untuk memberikan pelayanan pengiriman terbaik dan teraman yang menjangkau seluruh area Indonesia. Mobil akan diantarkan sesuai dengan kota tujuan Anda.</div><div><br></div><div>Garansi Mesin &amp; Transmisi 180 hari</div><div>Bebas Kecelakaan, Banjir</div><div>Dokumen Lengkap</div><div><br></div><div>Transmisi  = Automatic</div><div>Warna  = Putih</div><div>Kedaluarsa Pajak  = 04 Mar 2023</div><div>Kilometer  = 63.029 KM</div><div>BPKB Ada  = Ya</div><div>Tahun Produksi Kendaraan  = 2019</div><div>CC Mesin  = 1.500</div><div>Bahan Bakar  = Bensin</div><div><br></div><div>Melayani Pembelian via Cash atau kredit.</div><div><br></div><div>Cicilan Mulai dari Rp 9.5 jt/bln</div><div>Kelengkapan Data yang diperlukan  =</div><div>-Copy KTP Suami + Istri ( Jika Sudah Menikah )</div><div>-Copy Kartu Keluarga</div><div>-Copy PBB</div><div>-Rekening Listrik 3 bulan terakhir</div><div><br></div><div>Carro Automall</div><div>Jl. Harapan Indah Boulevard Commercial Park I Kavl CP 1, Kota Bekasi, Jawa Barat 17132</div><div><br></div><div>Carro Square Pondok Indah</div><div>Jl. Sultan Iskandar Muda No.41, RT.9/RW.4, Kby. Lama Utara, Kec. Kby. Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12240</div><div><br></div><div>Carro Automall Point</div><div>WTC Mall Matahari, Jl. Raya Serpong No.39, Pd. Jagung, Kec. Serpong,</div><div>Kota Tangerang Selatan, Banten 15326</div><div>Ruko No. 5863, Lobby Barat</div><div><br></div><div>Carro Automall Point Semarang</div><div>Semarang Town Square (Setos), Ground Floor</div><div>Jl. Inspeksi Jl. Gajahmada No.06, Kembangsari, Kec. Semarang Tengah, Kota Semarang, Jawa Tengah 50133</div><div><br></div><div>Buka Setiap Hari Senin - Jumat  = Pukul 9 Pagi s/d 5 Sore</div><div>Sabtu  = Pukul 9 Pagi s/d 3 Sore</div><div>Minggu  = Pukul 10 Pagi s/d 3 Sore</div><div><br></div><div>GMLQV2</div>",
       price = 463000000,
       images = listOf(
         "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1648433231874-1_datcfo_ijda4c.jpg",
@@ -31,26 +30,27 @@ object FakeProductList {
     ),
     Mobil(
       id = 2,
-      name = "DAIHATSU ASTREA",
-      description = "qpwoeipoqwie",
-      price = 25000000,
+      name = "DAIHATSU AYLA 1.0 X DELUXE",
+      price = 125000000,
       images = listOf(
-        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244541-1_ngvcsj_rznbm6.jpg"
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/img-p1648697935139-1_vksm5x_c8j8us.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1648697935139-2_dc2kcx_jmlsr5.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1648697935139-3_o0rwvx_x2vrcs.jpg",
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1648697935139-4_mkhfza_ghpmqy.jpg",
       ),
       fuel = "Bensin",
-      year = 2015,
-      color = "Hitam",
+      year = 2020,
+      color = "Putih",
       mileage = 12012,
       location = "TANGERANG",
       transmission = "Otomatis",
       seatCapacity = 1,
       engineCapacity = 29299,
-      sellerName = "ical testing microsite"
+      sellerName = "The king cobra"
     ),
     Mobil(
       id = 3,
       name = "TOYOTA COROLLA",
-      description = "mobil test corolla dx",
       price = 70000000,
       images = listOf(
         "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244527-1_wjuvrj_a7le28.jpg"
@@ -68,7 +68,6 @@ object FakeProductList {
     Mobil(
       id = 4,
       name = "SUZUKI APV GX",
-      description = "",
       price = 205000000,
       images = listOf(
         "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-198469-1_na1wqt_ldhfs7.png"
@@ -86,7 +85,6 @@ object FakeProductList {
     Mobil(
       id = 5,
       name = "HYUNDAI GRAND EUR XG AT",
-      description = "New Car",
       price = 700000000,
       images = listOf(
         "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244525-1_rphxjx_eumeeu.png"
@@ -104,10 +102,9 @@ object FakeProductList {
     Mobil(
       id = 6,
       name = "HYUNDAI HYUNDA AVEGA GX M/T",
-      description = "New Car",
       price = 700000000,
       images = listOf(
-        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244524-1_keo0ld_yjmvku.png"
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1645865597145-3_n1vw12_sfgavo.jpg"
       ),
       fuel = "Bensin",
       year = 2021,
@@ -122,7 +119,6 @@ object FakeProductList {
     Mobil(
       id = 7,
       name = "HONDA HR-V 1.8 PRESTIGE",
-      description = "New Car",
       price = 280000000,
       images = listOf(
         "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244105-2_svkeyh__1665113951487_151064_utp3zt.png",
@@ -142,8 +138,7 @@ object FakeProductList {
     ),
     Mobil(
       id = 8,
-      name = "TOYOTA AVANZA E M/T",
-      description = "",
+      name = "TOYOTA AVANZA E M/T X",
       price = 300000000,
       images = listOf(
         "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1657614319522-1_mxqm0o_p8gxd5.jpg",
@@ -162,12 +157,9 @@ object FakeProductList {
     Mobil(
       id = 9,
       name = "TOYOTA AVANZA G 1.3 M/T",
-      description = "",
       price = 250000000,
       images = listOf(
-        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1657614319522-1_mxqm0o_p8gxd5.jpg",
-        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1657614319522-1_mxqm0o_p8gxd5.jpg",
-        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1657614319522-1_mxqm0o_p8gxd5.jpg"
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1642667182559-1_bvnmex_g4ynrs.jpg"
       ),
       fuel = "Bensin",
       year = 2017,
@@ -182,7 +174,6 @@ object FakeProductList {
     Mobil(
       id = 10,
       name = "MITSUBISHI PAJEROSPRT 2.5D EA/T",
-      description = "",
       price = 400000000,
       images = listOf(
         "https://res.cloudinary.com/lelestaticassets/image/upload/v1671282917/Dicoding%20Final%20Project/img-p1657612346839-1_zv3hgd_zhhcrc.jpg",
@@ -202,7 +193,6 @@ object FakeProductList {
     Mobil(
       id = 11,
       name = "TOYOTA AVANZA 1.3 G",
-      description = "Testitng tambah monbil",
       price = 500000000,
       images = listOf(
         "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244346-1_s8w3ot_ctdfsw.jpg"
@@ -220,12 +210,9 @@ object FakeProductList {
     Mobil(
       id = 12,
       name = "TOYOTA AVANZA E M/T",
-      description = "",
       price = 600000000,
       images = listOf(
-        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244345-1_f6ovyx_ftenyn.jpg",
-        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244345-1_f6ovyx_ftenyn.jpg",
-        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244345-1_f6ovyx_ftenyn.jpg"
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1656514252624-1_uyrnsp_ti5b3t.jpg"
       ),
       fuel = "Bensin",
       year = 2014,
@@ -240,10 +227,9 @@ object FakeProductList {
     Mobil(
       id = 13,
       name = "TOYOTA AVANZA 1.3 E",
-      description = "tsesyting ical",
       price = 340000000,
       images = listOf(
-        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244345-1_f6ovyx_ftenyn.jpg"
+        "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1638147881171-1_p7kdqt_v9q9se.jpg"
       ),
       fuel = "Bensin",
       year = 2016,
@@ -258,7 +244,6 @@ object FakeProductList {
     Mobil(
       id = 14,
       name = "TOYOTA AVANZA S VVT-I M/T",
-      description = "",
       price = 500000000,
       images = listOf(
         "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1656562855640-3_akrezz_urg4hr.jpg",
@@ -278,7 +263,6 @@ object FakeProductList {
     Mobil(
       id = 15,
       name = "TOYOTA AVANZA 1.3 R",
-      description = "",
       price = 300000000,
       images = listOf(
         "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-p1656062953831-3_lyqbdh_wtprlb.jpg",
@@ -298,7 +282,6 @@ object FakeProductList {
     Mobil(
       id = 16,
       name = "TOYOTA VIOS G A/T",
-      description = "",
       price = 200000000,
       images = listOf(
         "https://res.cloudinary.com/lelestaticassets/image/upload/c_scale,h_560,dpr_auto,f_auto/Dicoding%20Final%20Project/img-244106-1_fiov5v_n0umqp.jpg"
--- app/src/main/java/com/dicoding/momobil/model/Mobil.kt
+++ app/src/main/java/com/dicoding/momobil/model/Mobil.kt
@@ -3,7 +3,6 @@ package com.dicoding.momobil.model
 data class Mobil(
   val id: Int,
   val name: String,
-  val description: String?,
   val price: Int,
   val images: List<String>,
   val fuel: String,
